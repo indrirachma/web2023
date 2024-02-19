212966ced10ca1934dfa5adc92e48c6933168daf
Indri Rachma <indr23101ti@student.nurulfikri.ac.id>
2024-02-19 15:25:47 +0700

    update

---
+++ PraktikumWEB2/array_asosiatif.php
@@ -0,0 +1,16 @@
+<?php
+$dataMahasiswa = [
+    "nama" => "Indri Rachma",
+    "alamat" => "Tenjolaya, Bogor",
+    "prodi" => "Teknik Informatika",
+    "semester" => 6,
+    "sudahLulus" => false,
+];
+
+echo $dataMahasiswa["nama"] . "<br>";
+echo $dataMahasiswa["alamat"] . "<br>";
+echo $dataMahasiswa["prodi"] . "<br>";
+echo $dataMahasiswa["semester"] . "<br>";
+echo $dataMahasiswa["sudahLulus"] . "<br>";
+echo "<br>";
+?>
