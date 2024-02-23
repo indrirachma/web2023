24b48a5a5c81bebd2c03068e5cf909bf7a1a04df
Indri Rachma <indr23101ti@student.nurulfikri.ac.id>
2024-02-23 16:42:37 +0700

    update

--- PraktikumWEB2/variabel.php
+++ PraktikumWEB2/variabel.php
@@ -12,12 +12,10 @@ $sudahLulus = false;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
     <p><?php echo $namaLengkap ?></p>
     <p><?php echo $jurusan ?></p>
     <p><?php echo $semester ?></p>
     <p><?php echo $sudahLulus ?></p>
 </body>
-
 </html>
