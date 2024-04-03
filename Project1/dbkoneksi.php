eb49783836431da261780dbb6d5cc73a3b74d4ad
indrirachma <indr23101@student.nurulfikri.ac.id>
2024-04-03 12:14:30 +0700

    update

--- Project1/dbkoneksi.php
+++ Project1/dbkoneksi.php
@@ -5,7 +5,7 @@ $dbname = 'dbpuskesmas'; // Ganti dengan nama database Anda
 $user = 'root'; // Ganti dengan nama pengguna database Anda
 $pass = ''; // Ganti dengan kata sandi database Anda
 
-$dsn = "mysql:host=$host;port=3307;dbname=$dbname";
+$dsn = "mysql:host=$host;port=3308;dbname=$dbname";
 
 // Buat koneksi Database
 $dbh = new PDO($dsn, $user, $pass);
